b9b08d6c6d973b28fc4a74a97e831ec416b0b22d
AL DEIRI Mohamad <mhdderi93@gmail.com>
2022-12-16 12:00:39 +0100

    exercice 13 il est ajuter

---
+++ exercice-13.php
@@ -0,0 +1,19 @@
+    <?php  //ordinateur.class.php
+    Class voiture{
+    private $_marque;
+    private $_model;
+    private $_nbporte;
+    
+    public function __construct($marque, $model, $nbporte){
+          $this->_marque = $marque;
+          $this->_model = $model;
+          $this->_nbporte = $nbporte;
+        
+        public function getMarque(){
+        $this->_marque=$marque;
+        $this->_marque;
+        $this->_marque;
+        }
+    }
+$poste =new Ordinateur("Peugeot");
+echo $poste->getMarque();
