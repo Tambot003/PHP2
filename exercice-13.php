be268a3841c96b337b53e2637d1781d97278a6d2
AL DEIRI Mohamad <mhdderi93@gmail.com>
2022-12-20 10:44:22 +0100

    yyyyyyy

--- exercice-13.php
+++ exercice-13.php
@@ -1,7 +1,7 @@
 <h1 style="color: #00FF00;  ">
     Exercice 13
 </h1>
-<h3 style="color: #00FF00; ">Créer une classe Voiture possédant les propriétés suivantes : marque, modèle, nbPortes et
+<h3 style="color: #00FF0; ">Créer une classe Voiture possédant les propriétés suivantes : marque, modèle, nbPortes et
 vitesseActuelle ainsi que les méthodes demarrer( ), accelerer( ) et stopper( ) en plus
 des accesseurs (get) et mutateurs (set) traditionnels. La vitesse initiale de chaque véhicule
 instancié est de 0. Une méthode personnalisée pourra afficher toutes les informations d’un
@@ -15,7 +15,7 @@ tests pour vérifier la cohérence de la classe Voiture. Vous devez afficher les
 suivants :</h3>
 <hr>
 
-<body style="background-color: #393e46;" > </body>
+
 
     <?php 
 //     Class voiture{
@@ -36,8 +36,10 @@ suivants :</h3>
 //     }
 // $poste =new Ordinateur("Peugeot");
 // echo $poste->getMarque();
-//-------------------------------------------------------------------------------
-//-------------------------------------------------------------------------------
+//____________________________________________________________
+
+
+
 
 // class Voiture {
 //     private $_brandname;
@@ -83,58 +85,218 @@ suivants :</h3>
 //   $result = new Voiture("pegeot", 408 , 5 , 50 , "démarr");
 
 //   echo $result;
-//-----------------------------------------------------------------------------
-//-----------------------------------------------------------------------------
+//____________________________________________________________
 
-class Voiture1 {
-  private $_brandname;
-  private $_model;
-  private $_nbport;
-  private $_vitesse;
-  private $_statut; // démarré ou stoppé
 
-  public function __construct($brandname, $model, $nbport, $vitesse, $statut) {
-    $this->_brandname =  $brandname;
-    $this->_model = $model;
-    $this->_nbport = $nbport;
-    $this->_vitesse = 0;
-    $this->_statut = $statut;
-  }
 
-  public function getBrandname() {
-        return $this->_brandname;
-  }
 
-  public function getmodel() {
-         return $this->_model;
-  }
 
-  public function getnbport() {
-         return $this->_nbport;
-  }
 
-  public function getvitesse() {
-         return $this->_vitesse;
-  }  
+// class Voiture {
+//   private $_brandname;
+//   private $_model;
+//   private $_nbport;
+//   private $_vitesse;
+//   private $_statut; // démarré ou stoppé
 
-  public function getstatute() {
-    return $this->_statut;
-  }  
+//   public function __construct($brandname, $model, $nbport, $vitesse, $statut) {
+//     $this->_brandname =  $brandname;
+//     $this->_model = $model;
+//     $this->_nbport = $nbport;
+//     $this->_vitesse = 0;
+//     $this->_statut = $statut;
+//   }
 
-  public function ralentir($vitesse) {
-      $this->_vitesse = $vitesse;
-  }
+//   public function getBrandname() {
+//         return $this->_brandname;
+//   }
 
-  public function __toString() {
-    return " ---  $this->_brandname Pegeut---$this->_model with  $this->_nbport door $this->_vitesse -- $this->_statut ";
+//   public function getmodel() {
+//          return $this->_model;
+//   }
+
+//   public function getnbport() {
+//          return $this->_nbport;
+//   }
+
+//   public function getstatute() {
+//     return $this->_statut;
+//   }  
+
+//   public function getvitesse() {
+//          return $this->_vitesse;
+//   }  
+
+
+
+//   public function __toString() {
+//     return " ---  $this->_brandname Pegeut---$this->_model with  $this->_nbport door $this->_vitesse -- $this->_statut ";
+//   }
+
+//   public function __toString() {
+//     return " ---  $this->_brandname citroien---$this->_model with  $this->_nbport door $this->_vitesse -- $this->_statut ";
+//   }
+  
+// }
+
+
+// $car1 = new Voiture("pegeot", 408 , 5 , 50 , "démarr");
+// echo $car1;
+
+// echo "<br>";
+
+// $car2 = new Voiture("Citroien", "C4"  , 3 ,"nondémarr");
+// echo $car2;
+
+
+//     // var_dump(Voiture);
+//____________________________________________________________
+
+
+
+class Voiture
+{
+    // Propriétés de la classe Voiture
+    private $marque;
+    private $modele;
+    private $nbPortes;
+    private $vitesseActuelle;
+    private $statut;
+
+    // Constructeur de la classe Voiture
+    public function __construct($marque, $modele, $nbPortes,$vitesseActuelle)
+    {
+        $this->marque = $marque;
+        $this->modele = $modele;
+        $this->nbPortes = $nbPortes;
+        $this->vitesseActuelle =0;
+        $this->statut = false;
+        
+    }
+
+    // Méthode demarrer()
+    public function demarrer()
+    {
+     if ($this->statut == false){
+      $this->statut = true;
+      echo "La voiture démarre.<br>";
+      } else {
+        echo "la voituer ne peux pas démarre.<br>";
+      }
+    }
+
+
+    // Méthode accelerer()
+    public function accelerer($vitesse)
+    {
+      if($this->statut == true)
+      {
+        $this->vitesseActuelle += $vitesse;
+        echo "La voiture accélère de " . $vitesse . " km/h.<br>La vitesse actuelle est de ".$this->vitesseActuelle. " km/h<br>";
+      }
+    }
+
+
+    // Méthode stopper()
+    public function stopper()
+    {
+        $this->vitesseActuelle = 0;
+        echo "La voiture s'arrête.<br>";
+    }
+
+
+    // Accesseur (get) pour la propriété marque
+    public function getMarque()
+    {
+        return $this->marque;
+    }
+
+
+    // Mutateur (set) pour la propriété marque
+    public function setMarque($marque)
+    {
+        $this->marque = $marque;
+    }
+
+
+    // Accesseur (get) pour la propriété modèle
+    public function getModele()
+    {
+        return $this->modele;
+    }
+
+
+    // Mutateur (set) pour la propriété modèle
+    public function setModele($modele)
+    {
+        $this->modele = $modele;
+    }
+
+
+    // Accesseur (get) pour la propriété nbPortes
+    public function getNbPortes()
+    {
+        return $this->nbPortes;
+    }
+
+
+    // Mutateur (set) pour la propriété nbPortes
+    public function setNbPortes($nbPortes)
+    {
+        $this->nbPortes = $nbPortes;
+    }
+
+
+    // Accesseur (get) pour la propriété vitesseActuelle
+    public function getVitesseActuelle()
+    {
+        return $this->vitesseActuelle;
+    }
+
+
+    // Mutateur (set) pour la propriété vitesseActuelle
+    public function setVitesseActuelle($vitesseActuelle)
+    {
+        $this->vitesseActuelle = $vitesseActuelle;
+    }
+
+
+    // Méthode personnalisée pour afficher toutes les informations d'une voiture
+    // public function afficherInformations()
+    // {
+    //     echo "Marque : " . $this->marque . "<br>";
+    //     echo "Modèle";
+    // }
+    public function afficherInfos()
+  {
+    echo "Marque : ".$this->marque."<br>";
+    echo "Modèle : ".$this->modele."<br>";
+    echo "Nombre de portes : ".$this->nbPortes."<br>";
+    echo "Vitesse actuelle : ".$this->vitesseActuelle." km/h  <br>";
   }
 }
-
-$car = new Voiture1("pegeot", 408 , 5 , 50 , "démarr");
-$car->ralentir(30);
-echo $car;
+// Pour tester cette classe, on peut utiliser les instructions suivantes :
 
 
-echo $car->getvitesse(); 
+// Création d'une voiture de marque Peugeot, modèle 408 et 5 portes
+$v1 = new Voiture("Peugeot", "408", 5,0);
+// Affiche les informations de la voiture
+$v1->afficherInfos();
+// Démarre la voiture
+$v1->demarrer();
 
 
+$v1->accelerer(25); 
+$v1->accelerer(15);
+
+    echo "<br>";
+// Accélère la voiture de 50 km
+
+  // Création d'une voiture de marque citroein, modèle C4 et 3 portes
+    $v2 = new Voiture("Citroien", "C4", 3,0);
+    // Affiche les informations de la voiture
+    $v2->afficherInfos();
+    // Démarre la voiture
+$v2->demarrer();
+    // Accélère la voiture de 50 km
+$v2->accelerer(0.2);
--- other.php
+++ other.php
@@ -1,83 +1,126 @@
 <?php
-class Voiture {
-  private $marque;
-  private $modele;
-  private $nbPortes;
-  private $vitesseActuelle;
-  
-  public function __construct($marque, $modele, $nbPortes) {
-    $this->marque = $marque;
-    $this->modele = $modele;
-    $this->nbPortes = $nbPortes;
-    $this->vitesseActuelle = 0;
-  }
-  
-  public function demarrer() {
-    if ($this->vitesseActuelle == 0) {
-      echo "La voiture démarre.";
-    } else {
-      echo "La voiture est déjà en marche.";
+
+class Voiture
+{
+    // Propriétés de la classe Voiture
+    private $marque;
+    private $modele;
+    private $nbPortes;
+    private $vitesseActuelle;
+
+    // Constructeur de la classe Voiture
+    public function __construct($marque, $modele, $nbPortes)
+    {
+        $this->marque = $marque;
+        $this->modele = $modele;
+        $this->nbPortes = $nbPortes;
+        $this->vitesseActuelle = 0;
     }
-  }
-  
-  public function accelerer($vitesse) {
-    $this->vitesseActuelle += $vitesse;
-    echo "La voiture accélère à $this->vitesseActuelle km/h.";
-  }
-  
-  public function ralentir($vitesse) {
-    $this->vitesseActuelle -= $vitesse;
-    if ($this->vitesseActuelle < 0) {
-      $this->vitesseActuelle = 0;
+
+    // Méthode demarrer()
+    public function demarrer()
+    {
+        echo "La voiture démarre.<br>";
     }
-    echo "La voiture ralentit à $this->vitesseActuelle km/h.";
-  }
-  
-  public function stopper() {
-    if ($this->vitesseActuelle == 0) {
-      echo "La voiture est déjà arrêtée.";
-    } else {
-      echo "La voiture s'arrête.";
-      $this->vitesseActuelle = 0;
+
+    // Méthode accelerer()
+    public function accelerer($vitesse)
+    {
+        $this->vitesseActuelle += $vitesse;
+        echo "La voiture accélère de " . $vitesse . " km/h.<br>";
     }
-  }
-  
-  public function getMarque() {
-    return $this->marque;
-  }
-  
-  public function setMarque($marque) {
-    $this->marque = $marque;
-  }
-  
-  public function getModele() {
-    return $this->modele;
-  }
-  
-  public function setModele($modele) {
-    $this->modele = $modele;
-  }
-  
-  public function getNbPortes() {
-    return $this->nbPortes;
-  }
-  
-  public function setNbPortes($nbPortes) {
-    $this->nbPortes = $nbPortes;
-  }
-  
-  public function getVitesseActuelle() {
-    return $this->vitesseActuelle;
-  }
-  
-  public function setVitesseActuelle($vitesseActuelle) {
-    $this->vitesseActuelle = $vitesseActuelle;
+
+    // Méthode stopper()
+    public function stopper()
+    {
+        $this->vitesseActuelle = 0;
+        echo "La voiture s'arrête.<br>";
+    }
+
+    // Accesseur (get) pour la propriété marque
+    public function getMarque()
+    {
+        return $this->marque;
+    }
+
+    // Mutateur (set) pour la propriété marque
+    public function setMarque($marque)
+    {
+        $this->marque = $marque;
+    }
+
+    // Accesseur (get) pour la propriété modèle
+    public function getModele()
+    {
+        return $this->modele;
+    }
+
+    // Mutateur (set) pour la propriété modèle
+    public function setModele($modele)
+    {
+        $this->modele = $modele;
+    }
+
+    // Accesseur (get) pour la propriété nbPortes
+    public function getNbPortes()
+    {
+        return $this->nbPortes;
+    }
+
+    // Mutateur (set) pour la propriété nbPortes
+    public function setNbPortes($nbPortes)
+    {
+        $this->nbPortes = $nbPortes;
+    }
+
+    // Accesseur (get) pour la propriété vitesseActuelle
+    public function getVitesseActuelle()
+    {
+        return $this->vitesseActuelle;
+    }
+
+    // Mutateur (set) pour la propriété vitesseActuelle
+    public function setVitesseActuelle($vitesseActuelle)
+    {
+        $this->vitesseActuelle = $vitesseActuelle;
+    }
+
+    // Méthode personnalisée pour afficher toutes les informations d'une voiture
+    public function afficherInformations()
+    {
+        echo "Marque : " . $this->marque . "<br>";
+        echo "Modèle";
+    }
+    public function afficherInfos()
+  {
+    echo "Marque : ".$this->marque."\n";
+    echo "Modèle : ".$this->modele."\n";
+    echo "Nombre de portes : ".$this->nbPortes."\n";
+    echo "Vitesse actuelle : ".$this->vitesseActuelle." km/h\n";
   }
 }
-$car = new Voiture("pegeot", 408 , 5 , 50 , "démarr");
-  public function afficherInformations($car) 
-    echo "Marque: $this->marque\n";
-    echo "Modèle: $this->modele\n";
-    echo "Nombre de portes: $this;
+// Pour tester cette classe, on peut utiliser les instructions suivantes :
+
+
+// Création d'une voiture de marque Peugeot, modèle 408 et 5 portes
+$v1 = new Voiture("Peugeot", "408", 5);
+
+// Affiche les informations de la voiture
+$v1->afficherInfos();
+
+// Démarre la voiture
+$v1->demarrer();
+
+// Accélère la voiture de 50 km
+
+  // Création d'une voiture de marque Peugeot, modèle 408 et 5 portes
+    $v1 = new Voiture("Peugeot", "408", 5);
     
-    //Panneau de configuration\Comptes d’utilisateurs\Gestionnaire d'identification
+    // Affiche les informations de la voiture
+    $v1->afficherInfos();
+    
+    // Démarre la voiture
+    $v1->demarrer();
+    
+    // Accélère la voiture de 50 km
+    
